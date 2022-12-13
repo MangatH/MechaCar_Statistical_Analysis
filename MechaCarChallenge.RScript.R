1b7bee98e0341d7c2c4ecc03ac249ed43c0e8f78
MangatH <111387025+MangatH@users.noreply.github.com>
2022-12-13 15:36:28 +0530

    Adding challenge part 3.

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -26,3 +26,17 @@ total_summary
 #Create a lot_summary dataframe using the group_by() and the summarize() functions.
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 lot_summary
+
+#Part 3: T-Tests on Suspension Coils
+
+#Using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+#Using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+#Lot1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+#Lot2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+#Lot3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
+
